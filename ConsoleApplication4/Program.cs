5d0a19159bc508bfc3f013603f91abb2ccb6f66e
FinistArx <FinistArx@gmail.com>
2016-06-24 16:42:48 +0300

    new

--- ConsoleApplication4/Program.cs
+++ ConsoleApplication4/Program.cs
@@ -10,16 +10,30 @@ namespace ConsoleApplication4
     {
         static void Main(string[] args)
         {
-        }
-        void Qsort(double[] random, int a, int b)
+            Console.WriteLine("Введите числa");
+            string temp = Console.ReadLine();
+            int a = Convert.ToInt32(temp);
+            Console.WriteLine("Введите числo");
+            string temp1 = Console.ReadLine();
+            int b = Convert.ToInt32(temp1);
+            string temp2 = Console.ReadLine();
+            int razmer = Convert.ToInt32(temp2);
+            double[] random = new double[razmer];
+            
+         }
+
+
+        static void Qsort(double[] random , ref int a, ref int b)
         {
-            double mid;
-            int x = a;
-            int y = b;
+            double mid ;
+            int x = a ;
+            int y = b ;
+        
+
 
             if (a > b || b > a)
             {
-                 mid = random[(x + y) / 2];
+                mid = random[(x + y) / 2];
                 if (random[a] < mid && random[b] >= mid)
                 {
                     x++;
@@ -35,14 +49,13 @@ namespace ConsoleApplication4
 
             if (x < a)
             {
-                Qsort(random,  a,  x);
+                Qsort( random, ref a, ref x);
             }
             if (y < b)
             {
-                Qsort(random,  b,  y);
+                Qsort(random,ref b, ref y);
             }
-            
-             
         }
+
     }
 }
